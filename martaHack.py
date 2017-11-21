fa1a418bf58008a598af37bc1d2be7bb5011f0a1
vramesh32 <vishesh.ramesh@gatech.edu>
2017-11-20 22:39:20 -0500

    Add files via upload

---
+++ martaHack.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import pymysql
+
+class MartaHack:
+    def __init__(self,homeWin):
+        self.db = pymysql.connect(host='academic-mysql.cc.gatech.edu',
+                             user='cs4400_Group_14',
+                             password='_MlrJUuF',
+                             db='cs4400_Group_14')
+        self.cursor = self.db.cursor()
+
+        self.bgColor1 = "SlateGray"
+        self.bgColor2 = "tan2"
+        self.fgColor1 = "burlywood"
+        self.fgColor2 = "burlywood1"
+
+        self.homeWin = homeWin
+        self.homeWin.protocol("WM_DELETE_WINDOW",self.endProgram)
+        self.homeWin.title("Log In")
+        self.homeWin.configure(bg=self.bgColor1)
+
+        topF = Frame(self.homeWin,bg=self.bgColor1)
+        topF.grid(row=0,column=0,pady=15,padx=10)
+        l1 = Label(topF,text="Username",bg=self.fgColor1)
+        l1.grid(row=0,column=0,sticky=NSEW,pady=5,padx=5)
+        l2 = Label(topF,text="Password",bg=self.fgColor1)
+        l2.grid(row=1,column=0,sticky=NSEW,pady=5,padx=5)
+        e1 = Entry(topF)
+        e1.grid(row=0,column=1,sticky=NSEW,pady=5,padx=5)
+        e2 = Entry(topF)
+        e2.grid(row=1,column=1,sticky=NSEW,pady=5,padx=5)
+
+        passF = Frame(self.homeWin,bg=self.bgColor1)
+        passF.grid(row=1,column=0,pady=10,padx=20)
+        b1 = Button(passF,text="Log In",bg=self.fgColor1,command=self.logIn)
+        b1.grid(row=0,column=0,sticky=NSEW,pady=5,padx=5)
+        b1=Button(passF,text="Register",bg=self.fgColor1,command=self.register)
+        b1.grid(row=1,column=0,sticky=NSEW,pady=5,padx=5)
+
+    def logIn(self):
+        pass
+        # add code and sql here to determine if credentials match an account
+        # if not, message an error back
+        # if so, ascertain admin or reg user
+        # call either self.passHome() or self.adminHome()
+
+
+    def register(self):
+        pass
+
+    def adminHome(self):
+        pass
+
+    def passHome(self):
+        pass
+
+    def endProgram(self):
+        try:
+            self.cursor.close()
+            self.db.commit()
+            self.db.close()
+        except:
+            pass
+        self.homeWin.destroy()
+
+
+win = Tk()
+MartaHack(win)
+win.mainloop()
