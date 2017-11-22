0356725b02ffbc3fe471bf902d830edca668e0f6
Vishesh Ramesh <vishesh.ramesh@gatech.edu>
2017-11-22 18:17:57 -0500

    test commit from ide

--- martaHack.py
+++ martaHack.py
@@ -1,16 +1,15 @@
 from tkinter import *
+from tkinter import messagebox
 import pymysql
 import hashlib
 
-
-
 class MartaHack:
     def __init__(self,homeWin):
-        self.db = pymysql.connect(host='academic-mysql.cc.gatech.edu',
-                             user='cs4400_Group_14',
-                             password='_MlrJUuF',
-                             db='cs4400_Group_14')
-        self.cursor = self.db.cursor()
+        # self.db = pymysql.connect(host='academic-mysql.cc.gatech.edu',
+        #                      user='cs4400_Group_14',
+        #                      password='_MlrJUuF',
+        #                      db='cs4400_Group_14')
+        # self.cursor = self.db.cursor()
 
         self.bgColor1 = "SlateGray"
         self.bgColor2 = "tan2"
@@ -51,7 +50,25 @@ class MartaHack:
         hashed = m.hexdigest()
         usr = self.userE.get()
         query = '''select isPassenger where username = ''' + usr + ''' and pass = ''' + hashed
-        pass
+        #c = self.cursor.execute(query)
+        #coding off network, instantiating variables
+        c = 1
+        if c == 0:
+            messagebox.showerror("Login failed. Check your spelling and try again")
+            pass
+        elif c > 1:
+            messagebox.showerror("MULTIPLE USERS HAVE THESE CREDENTIALS. I HAVE NO IDEA HOW THIS IS POSSIBLE, FUCK YOUR SHIT")
+            pass
+        else:
+            #isAdmin = list(cursor.fetchall())[0]
+            isAdmin = True
+            print(isAdmin)
+            if isAdmin:
+                self.homeWin.withdraw()
+                self.adminHome()
+            else:
+                self.homeWin.withdraw()
+                self.passHome()
         # add code and sql here to determine if credentials match an account
         # if not, message an error back
         # if so, ascertain admin or reg user
@@ -60,7 +77,44 @@ class MartaHack:
     def register(self):
         # withdraw home window and create a new register window
         # the register button must call a registerNewUser method
-        pass
+        self.homeWin.withdraw()
+        self.regWin = Toplevel()
+        self.regWin.protocol("WM_DELETE_WINDOW",self.endProgram)
+        self.regWin.title("Log In")
+        self.regWin.configure(bg=self.bgColor1)
+
+        topF = Frame(self.regWin,bg=self.bgColor1)
+        topF.grid(row=0,column=0,pady=15,padx=10)
+        l1 = Label(topF,text="Username",bg=self.fgColor1)
+        l1.grid(row=0,column=0,sticky=NSEW,pady=5,padx=5)
+        l2 = Label(topF,text="Email Address",bg=self.fgColor1)
+        l2.grid(row=1,column=0,sticky=NSEW,pady=5,padx=5)
+        e1 = Entry(topF)
+        e1.grid(row=0,column=1,sticky=NSEW,pady=5,padx=5)
+        e2 = Entry(topF)
+        e2.grid(row=1,column=1,sticky=NSEW,pady=5,padx=5)
+        l3 = Label(topF,text="Password",bg=self.fgColor1)
+        l3.grid(row=2,column=0,sticky=NSEW,pady=5,padx=5)
+        l4 = Label(topF,text="Confirm Password",bg=self.fgColor1)
+        l4.grid(row=3,column=0,sticky=NSEW,pady=5,padx=5)
+        e3 = Entry(topF)
+        e3.grid(row=2,column=1,sticky=NSEW,pady=5,padx=5)
+        e4 = Entry(topF)
+        e4.grid(row=3,column=1,sticky=NSEW,pady=5,padx=5)
+
+        botF = Frame(self.regWin,bg=self.bgColor1)
+        botF.grid(row=1,column=0,pady=15,padx=10)
+        self.v = StringVar()
+        self.v.set("False")
+        rb1 = Radiobutton(botF,text="Use my existing card",variable = self.v, value = "True",bg=self.fgColor1)
+        rb1.grid(row=0,column=0,columnspan=2,pady=5,padx=5,sticky=NSEW)
+        l5 = Label(botF,text= "Card Number",bg=self.fgColor1)
+        l5.grid(row=1,column=0,pady=5,padx=5,sticky=NSEW)
+        e5 = Entry(botF)
+        e5.grid(row=1,column=1,pady=5,padx=5,sticky=NSEW)
+        rb2 = Radiobutton(botF,text="Get a new breezecard",variable = self.v,value = "False",bg=self.fgColor1)
+        rb2.grid(row=2,column=0,pady=5,padx=5,columnspan=2,sticky=NSEW)
+
 
     def registerNewUser(self):
         # add code to check if entries are valid
@@ -68,12 +122,15 @@ class MartaHack:
         # if so, try to insert into db with mysql, catch pass
         # if all successfull, message "account created"
         # withdraw (destroy?) register win, call passHome if all successfull
+        self.regWin.destroy()
+        self.passHome()
         pass
 
     def adminHome(self):
         # create adminHome page
         # buttons to call self.adminStationMgt, self.adminSuspMgt,
             # self.adminCardMgt,self.pflowReport, self.logOut
+        self.adminHomeWin = Toplevel()
         pass
 
     def passHome(self):
